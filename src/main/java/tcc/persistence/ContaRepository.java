8152a2e109781815af3d889454be123b397c125d
Bruno Paulo Bender <69349855+BrunoBender@users.noreply.github.com>
2021-02-05 21:52:17 -0300

    cria o branch bruno

--- src/main/java/tcc/persistence/ContaRepository.java
+++ src/main/java/tcc/persistence/ContaRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+//repositório conta
 public interface ContaRepository extends JpaRepository<Conta, Long> {
 
     Optional<Conta> findBySenha(String senhaId);
