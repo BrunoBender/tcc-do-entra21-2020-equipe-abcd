0f95a7d2e7353fc896d8fcf1b2a6ddbefe86ab3e
Bruno Paulo Bender <69349855+BrunoBender@users.noreply.github.com>
2021-02-08 13:43:38 -0300

    adiciona API de validar usuario e pesquisa de produtos

--- src/main/java/tcc/persistence/CatalogoRepository.java
+++ src/main/java/tcc/persistence/CatalogoRepository.java
@@ -1,7 +1,15 @@
 package tcc.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import tcc.model.Catalogo;
+import tcc.model.Conta;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface CatalogoRepository extends JpaRepository<Catalogo, Long> {
+    @Query("Select u from Catalogo u where u.nomeDescritivo = :nomeDescritivo")
+    List<Catalogo> findByNomeDescritivo(@Param("nomeDescritivo") String nomeDescritivo);
 }
