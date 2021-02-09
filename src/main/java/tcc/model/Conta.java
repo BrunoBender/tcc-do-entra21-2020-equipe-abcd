d4342836acf05db1aa818d3fae022882130822a1
Klaus Ferreira <klaus@codewalkers.org>
2021-02-09 01:40:05 -0300

    adiciona length na anotacao column que tem string como tipo de dado

--- src/main/java/tcc/model/Conta.java
+++ src/main/java/tcc/model/Conta.java
@@ -4,37 +4,40 @@ import java.time.LocalDateTime;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import lombok.Data;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 
-@Data
 @Entity
 @Table(name="conta")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+@Data
 public class Conta {
 	@Column(name="conta_id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Id
 	private long contaId;
 
-	@Column(name="nome_usuario")
+	@Column(name="nome_usuario", length = 50)
 	@NotNull
 	private String nomeUsuario;
 
-	@Column(name="nome")
+	@Column(name="nome", length = 100)
 	@NotNull
 	private String nome;
 
-	@Column(name="telefone")
+	@Column(name="telefone", length = 15)
 	@NotNull
 	private String telefone;
 
-	@Column(name="cep")
+	@Column(name="cep", length = 10)
 	@NotNull
 	private String cep;
 
-	@Column(name="email")
+	@Column(name="email", length = 50)
 	@NotNull
 	private String email;
 
-	@Column(name="senha")
+	@Column(name="senha", length = 30)
 	@NotNull
 	private String senha;
 
