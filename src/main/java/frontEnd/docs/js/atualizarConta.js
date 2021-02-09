49cde0e1151eb73ca954439244ececb9c153a8b6
And <andre.pereira.martins100@gmail.com>
2021-02-09 00:48:31 -0300

    atualização api cadastro de conta comercial

--- src/main/java/frontEnd/docs/js/atualizarConta.js
+++ src/main/java/frontEnd/docs/js/atualizarConta.js
@@ -1,37 +1,38 @@
 function atualizar(){
     var comercial = {};
-    comercial.NomeFantasia = document.getElementById("idNomeFantasia").value;
-    comercial.Logo = document.getElementById("idLogo");
-    comercial.categoria = document.getElementById("idCatLoja").value;
+    //comercial.contaId = (usar a variavel global do user)  ;
+    comercial.nomeFantasia = document.getElementById("idNomeFantasia").value;
+    //comercial.Logo = document.getElementById("idLogo");
+    comercial.razaoSocial = document.getElementById("idCatLoja").value;
     comercial.cnpj = document.getElementById("idCnpj").value;
     comercial.cep = document.getElementById("idCep").value;
-    comercial.telefone = document.getElementById("idTel").value;
+   // comercial.telefone = document.getElementById("idTel").value;
 
-    console.log(comercial.NomeFantasia);
+    console.log(comercial.nomeFantasia);
     console.log(comercial.categoria);
     console.log(comercial.cnpj);
     console.log(comercial.cep);
     console.log(comercial.telefone);
 
-    //var json = JSON.stringify(usuario);
+    var json = JSON.stringify(comercial);
   
-   // var url = "http://localhost:8081/api/comercial/cria";
- //   var xhr = new XMLHttpRequest();
+    var url = "http://localhost:8081/api/comercios/cria";
+    var xhr = new XMLHttpRequest();
   
-    //xhr.open("POST", url, true);
+    xhr.open("POST", url, true);
   
-   // xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+   xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
   
- //   xhr.onload = function () {
-    //  var comercial = JSON.parse(xhr.responseText);
-    //  if (xhr.readyState == 4 && xhr.status == "201") {
-    //    console.table(comercial);
-    //  } else {
-    //    console.error(comercial);
-    //  }
+    xhr.onload = function () {
+      var comercio = JSON.parse(xhr.responseText);
+      if (xhr.readyState == 4 && xhr.status == "201") {
+    console.table(comercio);
+      } else {
+        console.error(comercio);
+      }
   
-    //  var form = document.getElementById("formCadastro");
-    //  form.reset();
-    //}
-    //xhr.send(json);
+      var form = document.getElementById("formCadastro");
+      form.reset();
+    }
+    xhr.send(json);
   }
