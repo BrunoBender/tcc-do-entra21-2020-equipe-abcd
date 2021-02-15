d54a27d3733431557ac8a84b259e4ba6e5ac52b9
Klaus Ferreira <klaus@codewalkers.org>
2021-02-15 13:21:44 -0300

    adiciona arquivo do pacote config

---
+++ src/main/java/tcc/config/WebMvcConfig.java
@@ -0,0 +1,16 @@
+package tcc.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import tcc.common.bind.DTOMethodProcessor;
+
+import java.util.List;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+		argumentResolvers.add(new DTOMethodProcessor());
+	}
+}
